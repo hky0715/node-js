08475d5fa140ac03d469de8cec4afd77f419e156
hky0715 <hky0715@gmail.com>
2024-05-30 09:39:29 +0900

    node.js commit

---
+++ file-repository.js
@@ -0,0 +1,6 @@
+function findAll(path) {
+    return "여기 있지롱!";
+}
+
+// findall함수를 노출하겠다고 함! exports는 노출할 객체, findAll은 속성..!
+exports.findAll = findAll;
--- first.js
+++ first.js
@@ -1,1 +1,6 @@
+// import하고싶은 녀석을 require해서 객체로 만듦. import할땐 파일의 경로까지 포함해야함
+let repository = require("./file-repository.js");
+
 console.log("Hello Nodejs!", global);
+
+console.log(repository.findAll());
